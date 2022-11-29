0b3908fe896b59fcc5323643ea2c5d0e6ebfc6d3
shohamroditimemphis <shoham@memphis.dev>
2022-11-29 17:14:36 +0200

    add support protobuf

--- main.go
+++ main.go
@@ -2,12 +2,17 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
+	"log"
+
 	"os"
+	"strings"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/memphisdev/memphis.go"
+	"google.golang.org/protobuf/proto"
 )
 
 func main() {
@@ -32,35 +37,106 @@ func main() {
 		}
 		var b body
 		bodyReq := c.Body()
-		err := json.Unmarshal(bodyReq, &b)
-		if err != nil {
-			return err
-		}
 
+		contentType := string(c.Request().Header.ContentType())
+		var message []byte
 		hdrs := memphis.Headers{}
 		hdrs.New()
+		caseText := strings.Contains(contentType, "text")
 
-		var headers map[string]string
-		err = json.Unmarshal([]byte(b.Headers), &headers)
-		if err != nil {
-			return err
+		if caseText {
+			contentType = "text/"
 		}
 
-		var k, v string
-		for key, value := range headers {
-			k = key
-			v = value
-
-			err = hdrs.Add(k, v)
+		switch contentType {
+		case "application/json":
+			err := json.Unmarshal(bodyReq, &b)
 			if err != nil {
 				return err
 			}
+
+			var headers map[string]string
+			err = json.Unmarshal([]byte(b.Headers), &headers)
+			if err != nil {
+				return err
+			}
+
+			var k, v string
+			for key, value := range headers {
+				k = key
+				v = value
+
+				err = hdrs.Add(k, v)
+				if err != nil {
+					return err
+				}
+			}
+
+			message, err = json.Marshal(b.Message)
+			if err != nil {
+				return err
+			}
+		case "text/":
+			err := json.Unmarshal(bodyReq, &b)
+			if err != nil {
+				return err
+			}
+			var headers map[string]string
+			err = json.Unmarshal([]byte(b.Headers), &headers)
+			if err != nil {
+				return err
+			}
+
+			var k, v string
+			for key, value := range headers {
+				k = key
+				v = value
+
+				err = hdrs.Add(k, v)
+				if err != nil {
+					return err
+				}
+			}
+
+			message, err = json.Marshal(b.Message)
+			if err != nil {
+				return err
+			}
+		case "application/x-protobuf":
+			msg := &Msg{}
+			err = proto.Unmarshal(bodyReq, msg)
+			if err != nil {
+				log.Fatal("unmarshaling error: ", err)
+			}
+
+			message, err = json.Marshal(msg.Message)
+			if err != nil {
+				return err
+			}
+
+			var headers map[string]string
+			err = json.Unmarshal([]byte(msg.Headers), &headers)
+			if err != nil {
+				fmt.Print(err)
+
+				return err
+			}
+
+			var k, v string
+			for key, value := range headers {
+				k = key
+				v = value
+
+				err = hdrs.Add(k, v)
+				if err != nil {
+					fmt.Print(err)
+					return err
+				}
+			}
+		default:
+			return errors.New("unsupported content type")
 		}
 
-		message, err := json.Marshal(b.Message)
-		if err != nil {
-			return err
-		}
 		if err := producer.Produce(message, memphis.MsgHeaders(hdrs)); err != nil {
 			fmt.Println(err.Error())
 			c.Status(400)
