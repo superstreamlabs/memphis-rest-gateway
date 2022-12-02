a56eca7560bd02c690082b46f9fec85a681b7095
idanasulinStrech <idan@memphis.dev>
2022-12-02 15:13:38 +0200

    connection establish loop

--- main.go
+++ main.go
@@ -4,8 +4,7 @@ import (
 	"fmt"
 	"http-proxy/conf"
 	"http-proxy/router"
-
-	"os"
+	"time"
 
 	"github.com/memphisdev/memphis.go"
 )
@@ -14,12 +13,23 @@ var configuration = conf.GetConfig()
 
 func main() {
 	configuration := conf.GetConfig()
-	conn, err := memphis.Connect(configuration.MEMPHIS_HOST, configuration.ROOT_USER, configuration.CONNECTION_TOKEN)
-	if err != nil {
-		fmt.Println(err.Error())
-		os.Exit(1)
+	var conn *memphis.Conn
+	ticker := time.NewTicker(1 * time.Second)
+	for {
+		select {
+		case <-ticker.C:
+			var err error
+			conn, err = memphis.Connect(configuration.MEMPHIS_HOST, configuration.ROOT_USER, configuration.CONNECTION_TOKEN)
+			if err == nil {
+				ticker.Stop()
+				goto serverInit
+			} else {
+				fmt.Printf("Awaiting to establish connection with Memphis - %v", err.Error())
+			}
+		}
 	}
 
+serverInit:
 	app := router.SetupRoutes(conn)
 	app.Listen(":" + configuration.HTTP_PORT)
 }
